3deef1086461fb484e52ba67e2c76e27e99a1e99
Reshma <resh2302@gmail.com>
2014-05-02 02:04:33 -0400

    program2 and nav

--- 03.website/v2/final-site_v2/js/main.js
+++ 03.website/v2/final-site_v2/js/main.js
@@ -10,6 +10,38 @@ $('#horMenuBars').click(function(){
     $('#r_horizontalMenuWrap').animate({'width': 'toggle'});
 });
 
-$('.iconImg').hover(function(){
-   $(this).next('.iconTxt').children('div').toggle(1000);
+$('.iconImg').mouseover(function(){
+   // $(this).next('.iconTxt').children('div').toggle(1000);
+   if($(this).attr('id') == "r_Home"){
+   		$('#menuName').text("HOME").fadeIn();	
+   }
+   else if($(this).attr('id') == "r_IT")
+   {
+   		$('#menuName').text("IT").fadeIn();	
+
+   }
+   else if($(this).attr('id') == "r_media")
+   {
+   		$('#menuName').text("MEDIA").fadeIn();	
+   	
+   }
+   else if($(this).attr('id') == "r_art")
+   {
+   		$('#menuName').text("ART").fadeIn();	
+   	
+   }
+   else if($(this).attr('id') == "r_about")
+   {
+   		$('#menuName').text("ABOUT").fadeIn();	
+   	
+   }
+   else if($(this).attr('id') == "r_contact")
+   {
+   		$('#menuName').text("CONTACT").fadeIn();	
+   	
+   }
+});
+
+$('.iconImg').mouseout(function(){
+	$('#menuName').fadeOut();
 });
